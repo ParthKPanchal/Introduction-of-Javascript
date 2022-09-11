6743557c1d9e2dfab31a5035eb689525b12d4559
Parth Panchal <panchalparth93@yahoo.in>
2022-09-11 17:45:42 +0530

    training

---
+++ Condition 2.js
@@ -0,0 +1,13 @@
+var food = "Fast Food";
+if( food == "Apple"){
+    alert("We ate some apple");
+}
+else if( food == "Meat"){
+    alert("We ate some meat");
+}
+else if( food == "Fast Food"){
+    alert("We ate some Fast Food");
+}
+else{
+    alert("We ate something else");
+}
